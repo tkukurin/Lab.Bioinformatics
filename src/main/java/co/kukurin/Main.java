d540d217f0a672602bcbf11e236dae57ceaadba4
Toni Kukurin <tkukurin@google.com>
2018-01-01 17:04:00 +0000

    reader from fasta files (https://github.com/yeastrc/java-fasta-utils)

--- src/main/java/co/kukurin/Main.java
+++ src/main/java/co/kukurin/Main.java
@@ -6,6 +6,8 @@ import co.kukurin.ReadMapper.CandidateRegion;
 import co.kukurin.ReadMapper.IndexJaccardPair;
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Multimap;
+import java.util.ArrayList;
+import java.util.stream.StreamSupport;
 import org.rabinfingerprint.polynomial.Polynomial;
 
 import java.io.IOException;
@@ -13,16 +15,25 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import org.yeastrc.fasta.FASTAEntry;
+import org.yeastrc.fasta.FASTAReader;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-//        String referenceRead = "ATTCGATCGATTACGATTCGATCGATTACGATTCGATCGATTACGATTCGATCGATTACG";
-//        String subread = "CGATTCCGATTCGAT";
-        String referenceRead = "ovo je neki reference dokument iz korpusa";
-        String subread = "neki document";
+        if (args.length != 2) {
+            System.out.println("Expected usage: [program] [reference FASTA file] [subread]");
+            System.exit(1);
+        }
 
-        ParameterSupplier parameterSupplier = ParameterSupplier.builder()
+        try {
+            FASTAReader referenceReader = FASTAReader.getInstance(args[0]);
+            List<FASTAEntry> entries = collectEntries(referenceReader);
+
+            FASTAReader subreadReader = FASTAReader.getInstance(args[1]);
+            List<FASTAEntry> subreads = collectEntries(subreadReader);
+
+            ParameterSupplier parameterSupplier = ParameterSupplier.builder()
                 .fingerprintingPolynomial(Polynomial.createIrreducible(53))
                 .stringToByteArrayConverter(String::getBytes)
                 .sketchSize(5)
@@ -30,18 +41,44 @@ public class Main {
                 .kmerSize(2)
                 .tau(0.1)
                 .build();
-        Hasher hasher = new Hasher(
-                parameterSupplier.getFingerprintingPolynomial(),
-                parameterSupplier.getStringToByteArrayConverter(),
-                parameterSupplier.getKmerSize());
-        Minimizer minimizer = new Minimizer(
-                parameterSupplier.getWindowSize());
-        Sketcher sketcher = new Sketcher(
-                parameterSupplier.getSketchSize());
-        ReadMapper readMapper = new ReadMapper(
-                parameterSupplier.getSketchSize(),
-                parameterSupplier.getTau());
 
+            outputSolution(parameterSupplier, entries, subreads);
+        } catch (Exception e) {
+            System.out.println(e.getLocalizedMessage());
+            System.exit(1);
+        }
+    }
+
+    private static void outputSolution(ParameterSupplier parameterSupplier,
+        List<FASTAEntry> entries, List<FASTAEntry> subreads) {
+        Hasher hasher = new Hasher(
+            parameterSupplier.getFingerprintingPolynomial(),
+            parameterSupplier.getStringToByteArrayConverter(),
+            parameterSupplier.getKmerSize());
+        Minimizer minimizer = new Minimizer(
+            parameterSupplier.getWindowSize());
+        Sketcher sketcher = new Sketcher(
+            parameterSupplier.getSketchSize());
+        ReadMapper readMapper = new ReadMapper(
+            parameterSupplier.getSketchSize(),
+            parameterSupplier.getTau());
+
+        for (FASTAEntry entry : entries) {
+            for (FASTAEntry subread : subreads) {
+               outputSolution(
+                   hasher, sketcher, minimizer, readMapper,
+                   entry.getSequence(), subread.getSequence());
+            }
+        }
+    }
+
+    private static void outputSolution(
+        Hasher hasher,
+        Sketcher sketcher,
+        Minimizer minimizer,
+        ReadMapper readMapper,
+        String referenceRead,
+        String subread) {
         List<Hash> indexHash = hasher.hash(referenceRead);
         List<Hash> readHash = hasher.hash(subread);
 
@@ -49,11 +86,11 @@ public class Main {
         List<MinimizerValue> sketches = sketcher.sketch(minimizerValues);
 
         List<CandidateRegion> candidateRegions = readMapper.collectCandidateRegions(
-                sketches.stream().map(MinimizerValue::getValue).collect(Collectors.toList()),
-                inverse(indexHash));
+            sketches.stream().map(MinimizerValue::getValue).collect(Collectors.toList()),
+            inverse(indexHash));
 
         List<IndexJaccardPair> result = readMapper.collectLikelySimilarRegions(
-                indexHash, readHash, candidateRegions);
+            indexHash, readHash, candidateRegions);
 
         result.stream().distinct().forEach(indexJaccardPair -> {
             System.out.println(indexJaccardPair);
@@ -61,6 +98,16 @@ public class Main {
             int index = indexJaccardPair.getIndex();
             System.out.println(referenceRead.substring(index, index + subread.length()));
         });
+    }
+
+    private static List<FASTAEntry> collectEntries(FASTAReader referenceReader) throws Exception {
+        List<FASTAEntry> entries = new ArrayList<>();
+        FASTAEntry entry = referenceReader.readNext();
+        while(entry != null) {
+          entries.add(entry);
+          entry = referenceReader.readNext();
+        }
+        return entries;
     }
 
     private static Map<Hash, Collection<Integer>> inverse(List<Hash> indexHash) {
